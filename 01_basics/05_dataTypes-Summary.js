f519c4eb18e716dc075bc55042e1b4f83f16581f
Abdul Mujeeb Khan <abdulmujeebkhan9871@gmail.com>
2024-09-05 17:09:29 +0000

    About types of Data Types

---
+++ 01_basics/05_dataTypes-Summary.js
@@ -0,0 +1,32 @@
+// ****Primitive Data Types (call-by-value)****
+// what this means is that whenever these are copied 
+// , reference to actually data is not given rather
+// a copy of it is given, so the changes are in copy
+// not on the actual data
+// 7 types : String, Number, Boolean, Null,
+//           undefined, Symbol, BigInt.
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId); //false
+
+// ****Reference type(Non-Primitive)(Call-by-reference)****
+// Arrays, Objects, Functions
+
+// *** JavaScript is a dynamically typed language
+// as we don't need to mention the data type while 
+// declaring a variable.
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let myObj = {
+    name: "Mujeeb",
+    age: 22
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof heros); //object 
+
+console.log(typeof anotherId);
