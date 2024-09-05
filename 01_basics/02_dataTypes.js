6703bb9ad0c342dfc92b98eb26b146ddf73d3c71
Abdul Mujeeb Khan <abdulmujeebkhan9871@gmail.com>
2024-09-05 12:43:05 +0000

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+
+let name = "Mujeeb"
+let age = 22
+let isLoggedIn = false
+
+// number = 2 to the power of 53
+// bigint 
+// string -> '' / ""
+// boolean -> true/false
+// null -> standalone value (representation of empty value)
+// undefined -> no value assigned
+// symbol -> mostly used in react(to find uniqueness)
+
+
+// object 
+console.log(typeof "Mujeeb")
+console.log(typeof age)
+
+console.log(typeof null)
+console.log(typeof undefined)
+
