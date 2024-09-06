c6ee11530e307c4df95fed0d3dba6f230fef52a1
Abdul Mujeeb Khan <abdulmujeebkhan9871@gmail.com>
2024-09-06 04:38:24 +0000

    String Operations and Manipulation

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,35 @@
+const name = "Mujeeb"
+const repoCount = 10
+console.log(name + repoCount + " Value"); // Outdated Syntax
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// for above syntax backticks (``) should be used 
+// instead of "" or ''.
+
+console.log("hello");
+const gameName = new String("Mujeeb-mk")
+
+console.log(gameName[1]); //access key - 1
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf('M'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+const newStringOne = "    Mujeeb        "
+console.log(newStringOne)
+console.log(newStringOne.trim());
+
+const url = "https://mujeeb.com/mujeeb%20khan"
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('mujeeb'));
+
+console.log(gameName.split('-'));
