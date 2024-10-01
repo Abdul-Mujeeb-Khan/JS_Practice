52af23d1f819d7f275de75e5bf68678aa1bc961b
Abdul Mujeeb Khan <abdulmujeebkhan9871@gmail.com>
2024-10-01 05:20:03 +0000

    conversions additional info

--- 01_basics/03_conversionsAndOperations.js
+++ 01_basics/03_conversionsAndOperations.js
@@ -50,10 +50,10 @@ let str2 = " Mujeeb"
 let str3 = str1 + str2
 console.log(str3);
 
-console.log("1" + 2); //12
-console.log(1 + "2"); //12
-console.log("1" + 2 + 2); //122
-console.log(1 + 2 + "2"); //32
+console.log("1" + 2, typeof("1" + 2)); //12
+console.log(1 + "2", typeof(1 + "2")); //12
+console.log("1" + 2 + 2, typeof("1" + 2 + 2)); //122
+console.log(1 + 2 + "2", typeof(1 + 2 + "2")); //32
 
 console.log(+true); //1 
 console.log(+""); //0
