d09c28f83463f030d7c9009567f7c92d8a29efb0
Abdul Mujeeb Khan <abdulmujeebkhan9871@gmail.com>
2024-10-02 04:19:33 +0000

    Number Methods

--- 01_basics/07_numbersAndMaths.js
+++ 01_basics/07_numbersAndMaths.js
@@ -43,3 +43,12 @@ const min = 10
 const max = 20
 
 console.log(Math.floor((Math.random() * (max - min + 1))) + min);
+
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_VALUE);
+console.log(Number.MIN_SAFE_INTEGER);
+
+
+
